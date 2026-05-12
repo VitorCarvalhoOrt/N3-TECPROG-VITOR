72e104920957daa1f12a22a8be30096feb0d3780
Vítor Carvalho <vitor.carvalho@ort.org.br>
2026-05-12 15:44:34 -0300

    menu inicio

--- Veiculos.py
+++ Veiculos.py
@@ -1,12 +1,12 @@
 class Veiculo:
-    def __init__(self, placa, marca, modelo, ano, valor_mercado):       
-        
-        if not (1885 < ano <=2026): # o delorean não funcionaria aqui...
+    def __init__(self, placa, marca, modelo, ano, valor_mercado):
+
+        if not (1885 < ano <= 2026):  # o delorean não funcionaria aqui...
             raise ValueError("Ano inválido")
-        
-        if not (10000 < valor_mercado <=200000):
+
+        if not (10000 < valor_mercado <= 200000):
             raise ValueError("Valor mercado invalido")
-        
+
         self.__placa = str(placa)
         self.__marca = str(marca)
         self.__modelo = str(modelo)
@@ -16,47 +16,57 @@ class Veiculo:
     @property
     def placa(self):
         return self.__placa
-    
+
     @property
     def marca(self):
         return self.__marca
-    
+
     @property
     def modelo(self):
         return self.__modelo
-    
+
     @property
     def ano(self):
         return self.__ano
-    
+
     @property
     def valor_mercado(self):
         return self.__valor_mercado
 
-    def calcular_imposto(self):# na função base, retorna 0
-        return 0 
-    
+    def calcular_imposto(self):  # na função base, retorna 0
+        return 0
+
     def __str__(self):
-            return(
-                f'Placa: {self.__placa} | Marca: {self.__marca} | Modelo: {self.__modelo}| Ano: {self.__ano} | Valor de Mercado: {self.__valor_mercado}'
-                )
-    
+        return f"Placa: {self.__placa} | Marca: {self.__marca} | Modelo: {self.__modelo} | Ano: {self.__ano} | Valor de Mercado: {self.__valor_mercado}"
+
+
 class Carro(Veiculo):
-    def __init__(self, placa, marca, modelo, ano, valor_mercado, numero_portas, tipo_carroceria):
-        
+    def __init__(
+        self, placa, marca, modelo, ano, valor_mercado, numero_portas, tipo_carroceria
+    ):
         if numero_portas not in [2, 4]:
             raise ValueError("Numero de portas inválido")
-        
         super().__init__(placa, marca, modelo, ano, valor_mercado)
         self.__numero_portas = int(numero_portas)
         self.__tipo_carroceria = str(tipo_carroceria)
-        
+
     def calcular_imposto(self):
         return self.valor_mercado * 0.1
-        
+
     def __str__(self):
-        return f'{super().__str__()} | Portas: {self.__numero_portas} | Carroceria: {self.__tipo_carroceria}'
-    
+        return f"{super().__str__()} | Portas: {self.__numero_portas} | Carroceria: {self.__tipo_carroceria}"
+
+
 class Moto(Veiculo):
-    def __init__(self, placa, marca, modelo, ano, valor_mercado):
+    def __init__(self, placa, marca, modelo, ano, valor_mercado, cilindrada, tipo_moto):
         super().__init__(placa, marca, modelo, ano, valor_mercado)
+        if cilindrada < 0:
+            raise ValueError("Cilindrada inválida")
+        self.__cilindrada = cilindrada
+        self.__tipo_moto = tipo_moto
+
+    def calcular_imposto(self):
+        return self.valor_mercado * 0.5
+
+    def __str__(self):
+        return f"{super().__str__()} | Cilindrada: {self.__cilindrada} | Tipo de Moto: {self.__tipo_moto}"
--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from Veiculos import Veiculo, Carro
+from Veiculos import Carro, Moto
 
-Corrola = Carro('ABC1243', 'Totota', 'Corola', 2001, 50000, 4, 'boa')
+entrada = None
+cadastrados = {}
 
-print(Corrola)
+while entrada != 0:
+    print('--------------MENU--------------')
+    print('Digite 0 para sair')
+    print('Digite 1 para cadastrar um carro')
+    print('Digite 2 para cadastrar uma moto')
+    print('Digite 3 para listar os veículos')
+    print('Digite 4 para calcular o imposto')
+
+    if entrada == 1:
+        placa          = input("digite a placa")
+        marca          = input("digite a placa")
+        modelo         = input("digite o modelo")
+        ano            = input("digite o ano")
+        valor_mercado  = input("digite o valor de mercado")
+        numero_portas  = input("digite o número de portas")
+        tipo_carroceria= input("digite o tipo de carroceria")
+        cadastrados[placa] = Carro(placa, marca, modelo, ano, valor_mercado, numero_portas, tipo_carroceria)
