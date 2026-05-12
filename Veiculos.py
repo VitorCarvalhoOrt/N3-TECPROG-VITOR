f3991e9799c2b3d28dd16b4cdc243559b77a79c8
Vítor Carvalho <vitor.carvalho@ort.org.br>
2026-05-12 15:08:34 -0300

    dedododeod

--- Veiculos.py
+++ Veiculos.py
@@ -2,10 +2,10 @@ class Veiculo:
     def __init__(self, placa, marca, modelo, ano, valor_mercado):       
         
         if not (1885 < ano <=2026): # o delorean não funcionaria aqui...
-            raise ValueError 
+            raise ValueError("Ano inválido")
         
         if not (10000 < valor_mercado <=200000):
-            raise ValueError
+            raise ValueError("Valor mercado invalido")
         
         self.__placa = str(placa)
         self.__marca = str(marca)
@@ -45,14 +45,18 @@ class Carro(Veiculo):
     def __init__(self, placa, marca, modelo, ano, valor_mercado, numero_portas, tipo_carroceria):
         
         if numero_portas not in [2, 4]:
-            raise ValueError
+            raise ValueError("Numero de portas inválido")
         
         super().__init__(placa, marca, modelo, ano, valor_mercado)
         self.__numero_portas = int(numero_portas)
         self.__tipo_carroceria = str(tipo_carroceria)
         
-        def calcular_imposto():
-            return self.__valor_mercado * 0.1
+    def calcular_imposto(self):
+        return self.valor_mercado * 0.1
         
-        def __str__(self):
-            return f'{super().__str__()} | Portas: {self.__numero_portas} | Carroceria: {self.__tipo_carroceria}'
+    def __str__(self):
+        return f'{super().__str__()} | Portas: {self.__numero_portas} | Carroceria: {self.__tipo_carroceria}'
+    
+class Moto(Veiculo):
+    def __init__(self, placa, marca, modelo, ano, valor_mercado):
+        super().__init__(placa, marca, modelo, ano, valor_mercado)
